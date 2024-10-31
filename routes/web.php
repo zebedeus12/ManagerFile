d9f27e79f62d2d3cadb31db899ba1792c8e47c97
Zebedeus12 <nabillazebedeus@gmail.com>
2024-10-31 11:31:27 +0700

    nambah web

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
-<<<<<<< HEAD
-use App\Http\Controllers\FolderController;
-=======
 use App\Http\Controllers\MediaController;
->>>>>>> 3aed01cb6b63cadc82dd0ce32ee6825d7160a4a7
+use App\Http\Controllers\FolderController;
 
 
 Route::get('/', function () {
@@ -32,14 +29,10 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 //employees
 Route::resource('employees', EmployeeController::class);
 
-<<<<<<< HEAD
 //folder
 Route::resource('folders', FolderController::class);
 Route::get('/folder/form', [FolderController::class, 'showForm'])->name('folder.form');
 Route::post('/folder/store', [FolderController::class, 'store'])->name('folder.store');
 
-
-=======
 //media
 Route::resource('media', MediaController::class);
->>>>>>> 3aed01cb6b63cadc82dd0ce32ee6825d7160a4a7
